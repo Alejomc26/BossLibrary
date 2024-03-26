69651d6062b4915659fb682de1092fa4ced56a6d
Alejomc26 <alechavezjaja26@gmail.com>
2024-03-25 19:04:59 -0500

    update build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,32 +1,58 @@
+import net.thebugmc.gradle.sonatypepublisher.PublishingType.USER_MANAGED
+
 plugins {
     id("java")
     id("net.thebugmc.gradle.sonatype-central-portal-publisher") version "1.2.3"
 }
 
+description = "Library for minecraft plugin developers (Paper api only)"
 group = "io.github.alejomc26"
 version = "1.0"
-
-repositories {
-    mavenCentral()
-    maven("https://repo.papermc.io/repository/maven-public/")
-}
 
 signing {
     useGpgCmd()
 }
 
 centralPortal {
+
+    publishingType = USER_MANAGED
+
+    jarTask = tasks.jar
+
+    sourcesJarTask = tasks.create<Jar>("sourcesEmptyJar"){
+        archiveClassifier = "sources"
+    }
+
+    javadocJarTask = tasks.create<Jar>("javadocEmptyJar") {
+        archiveClassifier = "javadoc"
+    }
+
     pom {
-        name = "Boss Library"
-        description = "Library for minecraft plugin developers (Paper api only)"
         url = "https://github.com/Alejomc26/BossLibrary"
         licenses {
             license {
-                name = "The Apache License, Version 2.0"
-                url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                name = "MIT License"
+                url = "http://www.opensource.org/licenses/mit-license.php"
             }
         }
+        developers {
+            developer {
+                name = "Alejo mc26"
+                email = "alechavezjaja26@gmail.com"
+            }
+        }
+        scm {
+            connection = "scm:git:https://github.com/Alejomc26/BossLibrary"
+            developerConnection = "scm:git:https://github.com/Alejomc26/BossLibrary"
+            url = "https://github.com/Alejomc26/BossLibrary"
+        }
+
     }
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
